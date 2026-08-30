2daae57894a87359a897711f03268443ed10edb8
Joshua Einstein-Curtis <jeinstei@gmail.com>
2026-08-30 23:19:50 +0200

    Added setting for mod science pack prerequisite exclusivity

--- prototypes/technologies.lua
+++ prototypes/technologies.lua
@@ -442,4 +442,4 @@ baketorio.setSciencePrerequisites = function (exclusive)
     end
 end
 
-baketorio.setSciencePrerequisites()
+baketorio.setSciencePrerequisites(settings.startup["baketorio-exclusive-science"].value)
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "baketorio-exclusive-science",
+        setting_type = "startup",
+        default_value = true
+    }
+}
+)
